b2aa45dfad69725df68d23ddbf8ed2d236c44ace
Cable <cable_x90@gmx.at>
2025-11-14 16:42:34 +0100

    icon

--- sw.js
+++ sw.js
@@ -4,7 +4,8 @@ const urlsToCache = [
   "/bootstrap-5.3.3/css/bootstrap.css",
   "/bootstrap-5.3.3/js/bootstrap.bundle.min.js",
   "/script.js",
-  "/img/shell.png"
+  "/img/shell.png",
+  "/manifest.json"
 ];
 
 self.addEventListener("install", event => {
@@ -16,5 +17,16 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
   );
 });
